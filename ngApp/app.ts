33bd03637e7528a09e048760a3016a99d595eebc
jdflemin <jdf@mchsi.com>
2017-04-13 13:31:32 -0700

    first commit

---
+++ ngApp/app.ts
@@ -0,0 +1,36 @@
+namespace socialmediaapp {
+
+    angular.module('socialmediaapp', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
+        $stateProvider: ng.ui.IStateProvider,
+        $urlRouterProvider: ng.ui.IUrlRouterProvider,
+        $locationProvider: ng.ILocationProvider
+    ) => {
+        // Define routes
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/ngApp/views/home.html',
+                controller: socialmediaapp.Controllers.HomeController,
+                controllerAs: 'controller'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: '/ngApp/views/about.html',
+                controller: socialmediaapp.Controllers.AboutController,
+                controllerAs: 'controller'
+            })
+            .state('notFound', {
+                url: '/notFound',
+                templateUrl: '/ngApp/views/notFound.html'
+            });
+
+        // Handle request for non-existent route
+        $urlRouterProvider.otherwise('/notFound');
+
+        // Enable HTML5 navigation
+        $locationProvider.html5Mode(true);
+    });
+
+    
+
+}
